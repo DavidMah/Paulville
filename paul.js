7438792143381bc8ebfa45a22f1afd8231500c9a
David Mah <Mahhaha@gmail.com>
2011-05-31 12:12:36 -0700

    build with resources

--- paul.js
+++ paul.js
@@ -1,8 +1,16 @@
-var updateTimer;
 $(document).ready(function() {
 	$(document).everyTime(100, 'adjust', adjustPanes, 0);
+	initializeControls();
 	initializeGame();
 });
+
+
+
+function initializeControls() {
+	$('.node').click(constructNode);
+	$('.turret').hide();
+	$('.construct_button').click(selectType);
+}
 
 function initializeGame() {
 	setResource('dereks', 100);
@@ -10,9 +18,18 @@ function initializeGame() {
 	setResource('bravos', 100);
 	$(document).everyTime(100, 'update', gameUpdate, 0);
 	$(document).attr('paused', false);
+	$(document).attr('selected', null);
+
+	var nodeData = [];
+	//name, damage, dereks, sacs, bravos
+	nodeData['basic_paul'] = (['Basic Paul', 10, 30, 30, 30]);
+	$(document).attr('node_data', nodeData);
 }
+/**************************************************
+*************Code for reorganizing the layout********
+***********************************************/
 function adjustPanes() {
-	if(!$(document).attr('paused') && (window.innerHeight < 800 || window.innerWidth < 800))
+	if(!$(document).attr('paused') && (window.innerHeight < 800 || window.innerWidth < 1050))
 		pauseGame('Window too Small. Please Resize');
 	if($('#footer').css('top') != (window.innerHeight - 300) + 'px') {
 		$('#footer').css('top', (window.innerHeight - 300) + 'px');
@@ -23,7 +40,17 @@ function adjustPanes() {
 		$('#footer').css('width', window.innerWidth + 'px');
 		$('#sky').css('width', window.innerWidth + 'px');
 		$('#stage').css('width', window.innerWidth + 'px');
+		$('#middle_bar').css('left', parseInt((window.innerWidth - 720) / 2) + 'px');
+		$('#controls').css('left', parseInt((window.innerWidth - 500) / 2) + 'px');
+		$('#left_pane').css('width', parseInt((window.innerWidth - 700) / 2) + 90 + 'px');
+		$('#right_pane').css('width', parseInt((window.innerWidth - 700) / 2) + 90 + 'px');
+		$('#left_pane_content').css('left', parseInt((window.innerWidth - 700) / 2) - 450 + 'px');
 	}
+}
+/*****************************************
+**********Code for controlling the game***********
+*******************************************/
+function gameUpdate() {
 }
 function pauseGame(reason) {
 	$(document).stopTime('update');
@@ -37,7 +64,8 @@ function returnToGame() {
 	$(document).everyTime(100, 'update', gameUpdate, 0);
 	$(document).attr('paused', false);
 }
-function gameUpdate() {
+function hasResource(resource, n) {
+	return $(document).attr(resource) >= n;
 }
 function setResource(resource, n) {
 	$(document).attr(resource, n);
@@ -46,4 +74,38 @@ function setResource(resource, n) {
 function addResource(resource, n) {
 	$(document).attr(resource, n + $(document).attr(resource));
 	$('#' + resource)[0].innerHTML = $(document).attr(resource);
+}
+function selectType() {
+	$('#selected_image').attr('src', 'images/' + $(this).attr('id') + '.png');
+	var nodeData = $(document).attr('node_data')[$(this).attr('type')];
+	var selectTable = $('#left_pane_content table td')
+	selectTable[0].innerHTML = nodeData[0];
+	selectTable[1].innerHTML = nodeData[1];
+	selectTable[2].innerHTML = nodeData[2];
+	selectTable[3].innerHTML = nodeData[3];
+	selectTable[4].innerHTML = nodeData[4];
+	$(document).attr('selected', $(this).attr('type'));
+}
+function constructNode() {
+	var selected = $(document).attr('selected');
+	if(!selected) {
+		return;
+	}
+	if(!subtractResources($(document).attr('node_data')[selected])) {
+		return;
+	}
+	$(this.children[0]).addClass(selected);
+	$(this.children[0]).show();
+}
+function subtractResources(necessary) {
+	var dereks = necessary[2];
+	var sacs = necessary[3];
+	var bravos = necessary[4];
+	if(!(hasResource('dereks', dereks) && hasResource('sacs', sacs) && hasResource('bravos', bravos))) {
+		return false;
+	}
+	addResource('dereks', -dereks);
+	addResource('sacs', -sacs);
+	addResource('bravos', -bravos);
+	return true;
 }
